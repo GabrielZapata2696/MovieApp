afec2f102fe2f41d878cd79faa8bc7f771e35796
Gabriel Zapata <sr.gabrielzm@gmail.com>
2021-09-18 10:00:08 -0500

    Adición modulo busqueda de peliculas

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { MoviesService } from '../services/movies.service';
+import { Pelicula } from '../interfaces/interfaces';
+import { DetalleComponent } from '../components/detalle/detalle.component';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -11,20 +14,41 @@ export class Tab2Page {
 
   textoBuscar: string = '';
   sugeridos: string[] = ['Spiderman', 'Avengers', 'El señor de los anillos', 'Batman']
+  resultPeliculas: Pelicula[] = [];
+  buscando = false;
+
 
   constructor(
-    private moviesService: MoviesService
+    private moviesService: MoviesService,
+    private modalCtrl: ModalController
   ) { }
 
 
   buscar(event) {
     const keyword = event.detail.value;
+    if (keyword.length === 0) {
+      this.buscando = false;
+      this.resultPeliculas = [];
+      return;
+    }
+    this.buscando = true;
     this.moviesService.getBuscarPeliculas(keyword.toString()).subscribe(peliculas => {
-      console.log(peliculas);
+      this.resultPeliculas = peliculas['results'];
+      this.buscando = false;
     });
+
+
   }
 
+  async verDetalle(id: number) {
+    const modal = await this.modalCtrl.create({
+      component: DetalleComponent,
+      componentProps: {
+        id,
+      },
+    });
 
-
+    modal.present();
+  }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   url: 'https://api.themoviedb.org/3',
-  api_key: 'apy_key',
+  api_key: '0837c9412db8bc68b9294d3715ec3eb2',
+  //api_key: 'api_key',
   imgPath: 'https://image.tmdb.org/t/p'
 };
