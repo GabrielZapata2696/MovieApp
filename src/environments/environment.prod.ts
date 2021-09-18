7bec2b628830af3e0bb4babaee50032dab949e65
Gabriel Zapata <sr.gabrielzm@gmail.com>
2021-09-17 20:48:49 -0500

    Corrección de seguridad - hotfix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   url: 'https://api.themoviedb.org/3',
-  api_key: '0837c9412db8bc68b9294d3715ec3eb2',
+  api_key: 'apy_key',
   imgPath: 'https://image.tmdb.org/t/p'
 };
